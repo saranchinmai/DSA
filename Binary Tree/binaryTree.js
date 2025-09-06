051840bb70a55885850be37caa5d17da617cdea5
Saran Pichika <saran.pichika@oracle.com>
2025-09-06 16:15:44 +0530

    Refactor TreeNode constructor and improve insert method for clarity

--- Binary Tree/binaryTree.js
+++ Binary Tree/binaryTree.js
@@ -3,8 +3,8 @@
  * the left and right nodes
  */
 class TreeNode {
-    constructor(value) {
-        this.value = value;
+    constructor(data) {
+        this.data = data;
         this.left = null;
         this.right = null;
     }
@@ -15,30 +15,76 @@ class BinaryTree {
         this.root = null;
     }
 
-    insert(value){
-        let node = new TreeNode(value)
-        if(this.root === null){
-            this.root = node
+    insert(value) {
+        let newNode = new TreeNode(value)
+        if (this.root === null) {
+            this.root = newNode
+        }
+        else {
+            this.insertNode(this.root, newNode);
+        }
+
+    }
+    insertNode(parent, newNode) {
+        if (parent.data < newNode.data) {
+            if (parent.right === null) {
+                parent.right = newNode
+            }
+            else {
+                this.insertNode(parent.right, newNode)
+            }
+        } else if (parent.data > newNode.data) {
+            if (parent.left === null) {
+                parent.left = newNode
+            }
+            else {
+                this.insertNode(parent.left, newNode)
+            }
+        }
+
+
+    }
+    delete(node) {
+        //code...
+    }
+
+    inOrderTraversal(node = this.root) {
+        if (node) {
+            this.inOrderTraversal(node.left)
+            console.log(node.data)
+            this.inOrderTraversal(node.right)
         }
 
     }
 
-    delete(node){
-        //code...
+    preOrderTraversal(node = this.root) {
+        if (node) {
+            console.log(node.data)
+            this.preOrderTraversal(node.left)
+            this.preOrderTraversal(node.right)
+        }
     }
 
-    inOrderTraversal(root){
-        //code...
-    }
-
-    preOrderTraversal(root){
-        //code...
-    }
-
-    postOrderTraversal(root){
-        //code...
+    postOrderTraversal(node = this.root) {
+        if (node) {
+            this.postOrderTraversal(node.left)
+            this.postOrderTraversal(node.right)
+            console.log(node.data)
+        }
     }
 }
 
-const tree = new BinaryTree();
-tree.insert(10)
+const bst = new BinaryTree();
+bst.insert(15);
+bst.insert(10);
+bst.insert(20);
+bst.insert(8);
+bst.insert(12);
+
+bst.preOrderTraversal()
+
+/** 
+ *             15
+ *          10    20
+ *         8  12
+ */
